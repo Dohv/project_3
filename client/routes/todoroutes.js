5bb4a4d93cfb74a48f215733744238673148ee52
ahmed t sajib <ahmedtsajib@gmail.com>
2017-05-24 10:30:33 -0400

    added Routes

---
+++ client/routes/todoroutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const tadaController = require('../controllers/tadaController');
+
+const todoRoutes = express.Router();
+
+todoRoutes.get('/', tadaController.index);
+todoRoutes.get('/:id', tadaController.show);
+todoRoutes.post('/', tadaController.create);
+todoRoutes.put('/:id', tadaController.update);
+todoRoutes.delete('/:id', tadaController.destroy);
+
+module.exports = todoRoutes;

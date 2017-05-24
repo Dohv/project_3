d0f2954ffcdf8b5057ed6d265639e05ade834267
dov stokar <dov.stokar@gmail.com>
2017-05-24 09:13:46 -0400

    fixed syntax in seed file

--- db/seeds/cities.sql
+++ db/seeds/cities.sql
@@ -1,8 +1,8 @@
 \c tada;
 
 INSERT INTO cities (name, img, zipcode) VALUES
-  ('New York', 'https://i.ytimg.com/vi/Fl7V5dX5IrM/maxresdefault.jpg', 10025)
-  ('Chicago', 'http://www.essexinn.com/d/essexinn/media/Attractions/3583017878_ee2e8e3d8b_b_1423674445483.jpg', 60601 )
-  ('Denver', 'http://www.blog.colormarketing.org/wp-content/uploads/2016/05/iStock_000083228293_Large.jpg', 80201)
-  ('Miami', 'https://www.discotech.me/wp-content/uploads/2014/11/miami.jpg', 33111);
+  ('New York', 'https://i.ytimg.com/vi/Fl7V5dX5IrM/maxresdefault.jpg', 10025),
+  ('Chicago', 'http://www.essexinn.com/d/essexinn/media/Attractions/3583017878_ee2e8e3d8b_b_1423674445483.jpg', 60601 ),
+  ('Denver', 'http://www.blog.colormarketing.org/wp-content/uploads/2016/05/iStock_000083228293_Large.jpg', 80201),
+  ('Miami', 'https://www.discotech.me/wp-content/uploads/2014/11/miami.jpg', 33111),
   ('San Francisco', 'https://static.wixstatic.com/media/771898_bef8adf8d5ba490fb6b73043a3c97138.jpg_srz_1903_1189_85_22_0.50_1.20_0.00_jpg_srz', 94103);

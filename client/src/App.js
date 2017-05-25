57082e285b2789208609d2932ec47b665d3bd6a9
dov stokar <dov.stokar@gmail.com>
2017-05-25 17:28:07 -0400

    react router npm

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 import TodoModle from './components/TodoModle';
 
@@ -7,13 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
         <TodoModle />
       </div>
     );

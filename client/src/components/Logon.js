63e5b6b94f63d58a4104cee7a8bff91b75b6e1c0
dov stokar <dov.stokar@gmail.com>
2017-05-28 12:39:20 -0400

    logon styling

--- client/src/components/Logon.js
+++ client/src/components/Logon.js
@@ -6,26 +6,28 @@ class Logon extends Component {
   render () {
     return (
       <div id='login'>
-        <form id='login-form' onSubmit={this.props.handleFormChange}>
-          <input 
-          type='text' 
-          name='name' 
-          placeholder='Name'
-          className='name'
-          />
-           
-          <br /><br />
-          <select name='city'>
-          <option>Select City</option>
-          {this.props.cities.map((city) => {
-            return (
-              <option value={city.name} key={city.id}>
-                {city.name}
-              </option>
-            )
-          })}
-        </select>
-        </form>
+        <div className='layer'>
+          <form id='login-form' onSubmit={this.props.handleFormChange}>
+            <input 
+            type='text' 
+            name='name' 
+            placeholder='Name'
+            className='name'
+            />
+            
+            <br /><br />
+            <select name='city'>
+            <option>Select City</option>
+            {this.props.cities.map((city) => {
+              return (
+                <option value={city.name} key={city.id}>
+                  {city.name}
+                </option>
+              )
+            })}
+          </select>
+          </form>
+        </div>
       </div>
     )
   }
